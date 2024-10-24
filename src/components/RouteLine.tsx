e85ea6ec0a7ddb25e23425c34cca765d81339fb4
Frankie Duck <frankie.duck@intellisense.io>
2024-10-24 21:10:31 +0100

    route line fixed

--- src/components/RouteLine.tsx
+++ src/components/RouteLine.tsx
@@ -2,30 +2,26 @@ import React from "react";
 import { Polyline } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
 
-type CoordinatesType = [string, string][];
+interface CoordinatesWrapper {
+  coordinates: [string, string][];
+}
+
 interface RouteLineProps {
-  routeCoordinates: CoordinatesType;
+  routeCoordinates: CoordinatesWrapper;
 }
 
 export default function RouteLine({ routeCoordinates }: RouteLineProps) {
   const convertStringCoordinatesToNumbers = (
-    coordinates: CoordinatesType
+    coordinatesObj: CoordinatesWrapper
   ): LatLngTuple[] => {
-    return coordinates.map((coord) => {
-      const [latitude, longitude] = coord as [string, string];
-      const parsedLatitude = parseFloat(latitude as string);
-      const parsedLongitude = parseFloat(longitude as string);
-  
-      if (!isNaN(parsedLatitude) && !isNaN(parsedLongitude)) {
-        return [parsedLatitude, parsedLongitude];
-      } else {
-        return [0, 0]; 
-      }
+    return coordinatesObj.coordinates.map((coord) => {
+      const [latitude, longitude] = coord;
+      return [parseFloat(latitude), parseFloat(longitude)] as LatLngTuple;
     });
   };
 
-  const numericCoordinates =
-    convertStringCoordinatesToNumbers(routeCoordinates);
+  const numericCoordinates = convertStringCoordinatesToNumbers(routeCoordinates);
+
   return (
     <Polyline pathOptions={{ color: "blue" }} positions={numericCoordinates} />
   );
